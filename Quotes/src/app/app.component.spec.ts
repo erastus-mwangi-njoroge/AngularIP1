523b9d5b5fedb5c44d88cf5cc3fa8fade80fea3c
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-08-23 20:39:55 +0300

    add changes

--- Quotes/src/app/app.component.spec.ts
+++ Quotes/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Quotes'`, () => {
+  it(`should have as title 'The-Quote-Application'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Quotes');
+    expect(app.title).toEqual('The-Quote-Application');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Quotes app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('The-Quote-Application app is running!');
   });
 });
